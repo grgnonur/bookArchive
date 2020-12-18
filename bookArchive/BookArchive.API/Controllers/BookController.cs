5a68f0c7efa0f785c298c163454d8d47586fba5b
grgnonur <kskonurgurgen@hotmail.com>
2020-12-18 18:20:25 +0300

    API
    bookArchiveAPI kısmı yapıldı CRUD işlemler tamamlandı.

---
+++ bookArchive/BookArchive.API/Controllers/BookController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bookArchive.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookArchive.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        Context context = new Context();
+        [HttpGet]
+        public List<Book> Get()
+        {
+            var list = context.Books.Include(x => x.Category).ToList();
+            return list;
+        }
+
+        [HttpGet("{id}")]
+        public Book Get(int id)
+        {
+            var book = context.Books.Find(id);
+            return book;
+        }
+        [HttpPost]
+        public Book Post([FromBody] Book book)
+        {
+            var temp = context.Categories.Where(x => x.id == book.Category.id).FirstOrDefault();
+            book.Category = temp;
+            context.Books.Add(book);
+            context.SaveChanges();
+            return book;
+            
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            context.Books.Find(id);
+        }
+
+        [HttpPut]
+        public Book Put([FromBody]Book book)
+        {
+            var temp = context.Books.Find(book.id);
+            temp.bookName = book.bookName;
+            temp.author = book.author;
+            var temp2 = context.Categories.Where(x => x.id == book.Category.id).FirstOrDefault();
+            temp.Category.id = temp2.id;
+            temp.description = book.description;
+            context.SaveChanges();
+            return book;
+        }
+
+    }
+}
---
+++ bookArchive/BookArchive.API/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bookArchive.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookArchive.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        Context context = new Context();
+        [HttpGet]
+        public List<Category> Get()
+        {
+            var degerler = context.Categories.ToList();
+            return degerler;
+        }
+        [HttpGet("{id}")]
+        public Category Get(int id)
+        {
+            var cat =context.Categories.Find(id);
+            return cat;
+        }
+
+        [HttpPost]
+        public Category Post([FromBody]Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return category;
+        }
+        [HttpPut]
+        public Category Put([FromBody] Category category)
+        {
+            var cat = context.Categories.Find(category.id);
+            cat.categoryName = category.categoryName;
+            context.SaveChanges();
+            return category;
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            context.Categories.Find(id);
+        }
+    }
+}
